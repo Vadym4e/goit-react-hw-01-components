fd6a4d6b6d23e34f7aeeb7556dae5dc2a8189afa
Vadym4e <vadym.o92@gmail.com>
2023-04-23 20:28:19 +0300

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Profile } from './Profile/Profile';
+import user from './Profile/user';
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data';
+import { FriendList } from './friendList/FriendList';
+import friends from './friendList/friends.json';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
+import transactions from './transactionHistory/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div class="container">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
